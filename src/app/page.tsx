9a6847dd7270b346e2b79ad99da517ab25e3bde7
Tyler Yang <tsyang59@gmail.com>
2024-05-03 19:28:09 -0700

    slight fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Translator from '../../components/translator';
 
 export default function Home() {
   return (
-    // Make a basic homepage with a title saying "Scribe, we need to center it and make it bigger"
-    // Make the background color scheme a slightly blue gradient
-    // Make the text color white
-    // Make the font a sans-serif font
     <main className= "bg-gradient-to-r from-blue-400 to-blue-500 h-screen flex flex-col justify-center items-center">
       <div >
         <h1 className="text-white text-6xl font-sans">Scribe</h1>
